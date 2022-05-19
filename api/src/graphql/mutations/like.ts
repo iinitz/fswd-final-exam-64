496007b73e48797a2403df89218e3ffc0655e1c7
l3luescreen <hacky404@hotmail.com>
2022-05-19 12:34:01 +0700

    Implement some of mutaions

--- api/src/graphql/mutations/like.ts
+++ api/src/graphql/mutations/like.ts
@@ -1,2 +1,15 @@
+import { ResolverResolveParams, ResolverRpCb } from 'graphql-compose'
+
+import { LikeTC } from '../../models/like'
+import { IApolloContext } from '../../types'
+import { ILike } from '../../types/models'
+
 // API: Implement resolver like with userId from context.user._id (Example in src/graphql/mutations/follower.ts)
+// export const addLike = LikeTC.mongooseResolvers.updateById().wrapResolve((next: ResolverRpCb<ILike, IApolloContext>) => (rp: ResolverResolveParams<ILike, IApolloContext>) => {
+//     if (rp.args?._id !== rp.context?.user._id) {
+//         return next(rp) as Promise<ILike>
+//     }
+//     return null
+// })
+
 // API: Implement resolver unlike with userId from context.user._id (Example in src/graphql/mutations/follower.ts)
--- api/src/graphql/mutations/user.ts
+++ api/src/graphql/mutations/user.ts
@@ -1,4 +1,12 @@
-import { schemaComposer } from 'graphql-compose'
+import { schemaComposer, ResolverResolveParams } from 'graphql-compose'
+import { mergeDeepRight } from 'ramda'
+
+import { verifyPassword } from '../../lib/passwordUtils'
+import { UserTC, UserModel } from '../../models/user'
+import { IApolloContext } from '../../types'
+import { ILoginArgs } from '../../types/graphql'
+import { IUser } from '../../types/models'
+
 
 const LoginPayloadOTC = schemaComposer.createObjectTC({
   name: 'LoginPayload',
@@ -22,4 +30,54 @@ const LoginPayloadOTC = schemaComposer.createObjectTC({
     - return token and message "Login success"
     - if error return error message "Server error"
 */
+
+export const login = UserTC.schemaComposer.createResolver({
+  name: 'login',
+  kind: 'mutation',
+  args: {
+    username: 'String',
+    password: 'String',
+  },
+  resolve: async ({ args }) => {
+    const username = args.username as string
+    const password = args.password as string
+
+    const user = await UserModel?.findOne({ username: username.toLowerCase() }) as IUser
+    if (!user) {
+      throw new Error(`Username ${username} not found`)
+    }
+    // const validPassword = await verifyPassword(user, password)
+    // if (!validPassword) {
+    //   throw new Error(`Incorrect password`)
+    // }
+  },
+})
+
+// export const login = UserTC.mongooseResolvers.createOne().wrapResolve((next) => async (rp) => {
+//   const username = rp.args?.record.username as string
+//   const target = await UserModel.findOne({ username })
+
+//   // const newRp = mergeDeepRight(rp, customRp) as ResolverResolveParams<ILoginArgs, IApolloContext>
+
+//   if (!target) {
+//     return next(rp) as Promise<ILoginArgs>
+//   }
+// })
+
 // API: Implement resolver register using createOne from UserTC
+
+export const register = UserTC.mongooseResolvers.createOne({ record: { requiredFields: ['username', 'password'] } }).wrapResolve((next) => async (rp) => {
+  const username = rp.args?.record.username as string
+  const target = await UserModel.findOne({ username })
+  const customRp: Partial<ResolverResolveParams<ILoginArgs, IApolloContext>> = {
+    args: {
+      filter: {
+        username: {
+          $ne: target,
+        },
+      },
+    },
+  }
+  const newRp = mergeDeepRight(rp, customRp) as ResolverResolveParams<ILoginArgs, IApolloContext>
+  return next(newRp) as Promise<ILoginArgs>
+})
