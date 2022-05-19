b54440fc206a211311e32cd5e3701f6516257d10
it62070180 <56427172+it62070180@users.noreply.github.com>
2022-05-19 12:47:20 +0700

    del not use

--- api/src/graphql/mutations/like.ts
+++ api/src/graphql/mutations/like.ts
@@ -1,2 +1,4 @@
 // API: Implement resolver like with userId from context.user._id (Example in src/graphql/mutations/follower.ts)
+export const like = ILike.mongooseResolverscreateOne({ record: { removeFields: ['_id', 'userId', 'createdAt', 'updatedAt'] } }).wrapResolve((next: ResolverRpCb<IFollower, IApolloContext>) => (rp: ResolverResolveParams<IFollower, IApolloContext>)
+
 // API: Implement resolver unlike with userId from context.user._id (Example in src/graphql/mutations/follower.ts)
